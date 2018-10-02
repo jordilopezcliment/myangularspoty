207a4ab602b02196b1519cef5cc187a9e5dae471
Jorge <ershava@gmail.com>
2018-10-02 22:05:59 +0200

    continuar con la navbar, hacer tests con routes y empezar con la primera conexión http

--- spotiapp/src/app/app.module.ts
+++ spotiapp/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ROUTES } from './app.routes';
 
+// Importar Rutas
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
